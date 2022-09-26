c580856bdf1eb8ace33d697c311ac1b99bdc3a69
Onaysa Ali <anisa.ali.5999385@gmail.com>
2022-09-26 16:30:20 -0400

    added some changes to the managerhtml file

---
+++ Develop/src/managerHtml.js
@@ -0,0 +1,16 @@
+function manageHtml(manager){
+    return `  <div class="col-sm-4 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Manger</h5>
+        <ul class="list-group">
+          <li class="list-group-item">name: </li>
+          <li class="list-group-item">id:  </li>
+          <li class="list-group-item">email: ploy3_98@yahoo.com</li>
+          <li class="list-group-item">OfficeNumber: 123</li>
+           
+        </ul>
+      </div>
+    </div>
+  </div>`
+  }
