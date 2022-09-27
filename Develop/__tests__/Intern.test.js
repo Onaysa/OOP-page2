9b10d07ede1294ab4e7fc33466e376e68dab5bce
Onaysa Ali <anisa.ali.5999385@gmail.com>
2022-09-26 22:14:24 -0400

    completed manager.test.js file

---
+++ Develop/__tests__/Intern.test.js
@@ -0,0 +1,18 @@
+const Intern = require("../lib/Intern")
+test("Can set school via constructor", () => {
+    const testValue = "NYU";
+    const e = new Intern("Ani", 1, "email@email.com", testValue);
+    expect(e.school).toBe(testValue);
+  });
+  
+  test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Ani", 1, "email@email.com", "NYU");
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Can get school via getSchool()", () => {
+    const testValue = "NYU";
+    const e = new Intern("Ani", 1, "email@email.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+  });
