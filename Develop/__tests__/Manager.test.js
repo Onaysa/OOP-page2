9f4a95f306bd632fcacf115900fb4af5b8b71c9f
Onaysa Ali <anisa.ali.5999385@gmail.com>
2022-09-26 22:20:03 -0400

    completed manager.test.js file

--- Develop/__tests__/Manager.test.js
+++ Develop/__tests__/Manager.test.js
@@ -1,2 +1,19 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
+test("Set office number via constructor argument", () => {
+    const testValue = 100;
+    const e = new Manager("Ani", 1, "email@email.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+  });
+  
+  test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Ani", 1, "emailt@email.com", 100);
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Get office number via getOffice()", () => {
+    const testValue = 100;
+    const e = new Manager("Ani", 1, "email@email.com", testValue);
+    expect(e.getOffice()).toBe(testValue);
+  });
